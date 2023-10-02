cd7fc047bc86181c47996fc82b710beb5d10ec27
Luis <luismingati@gmail.com>
2023-10-02 19:24:18 -0300

    db tambem

--- src/colaborator/dto/create-colaborator.dto.ts
+++ src/colaborator/dto/create-colaborator.dto.ts
@@ -1,10 +1,10 @@
-import {Prisma} from '@prisma/client';
-import { IsNotEmpty, IsString, IsNumber, IsOptional, IsInt, Min, Max } from 'class-validator';
-export class CreateColaboratorDto implements Prisma.ColaboratorCreateInput{
-    @IsNotEmpty()
-    @IsString()
-    name: string;
-    @IsNotEmpty()
-    @IsString()
-    area : string;
+import { Prisma } from '@prisma/client';
+import { IsNotEmpty, IsString } from 'class-validator';
+export class CreateColaboratorDto implements Prisma.ColaboratorCreateInput {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+  @IsNotEmpty()
+  @IsString()
+  area: string;
 }
